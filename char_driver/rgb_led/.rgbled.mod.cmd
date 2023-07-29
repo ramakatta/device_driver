9f46420f906cfc3702f74f4e16c0274143908788
ramakatta <85860239+ramakatta@users.noreply.github.com>
2023-07-29 16:59:20 +0100

    Delete .rgbled.mod.cmd

--- char_driver/rgb_led/.rgbled.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/rama/Downloads/Manipal/kernel_space/raspberry/rgb_led/rgbled.mod := printf '%s\n'   rgbled.o | awk '!x[$$0]++ { print("/home/rama/Downloads/Manipal/kernel_space/raspberry/rgb_led/"$$0) }' > /home/rama/Downloads/Manipal/kernel_space/raspberry/rgb_led/rgbled.mod
